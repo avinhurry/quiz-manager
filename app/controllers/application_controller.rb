3878d2a91dbbc561dcee61f8595a43974386e88f
avinhurry <avin.hurry@digital.education.gov.uk>
2021-09-20 18:56:44 +0100

    Add logic for three different user permissions
    - Add user_type column to users (string)
    - Add user_type to permitted params
    - Add user type radio button selection on registration page
    - Start restricting view depending on user_type

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,8 @@
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!
-  private
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+private
 
   # Overwriting the sign_out redirect path method
   def after_sign_out_path_for(resource_or_scope)
@@ -10,5 +12,10 @@ class ApplicationController < ActionController::Base
 # Overwriting the sign in redirect path method
   def after_sign_in_path_for(resource)
     quizzes_path
+  end
+
+  # Adding user_type to the list of permitted params
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:user_type])
   end
 end
